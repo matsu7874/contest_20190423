3c3d22801b069f87cef28805ef2270883da86eaa
endo <hiroyuki_endo@forcia.com>
2019-04-21 21:19:24 +0900

    tansan 解答のインデックスがずれていたので訂正

--- tansan/hiroendore_AC/main.py
+++ tansan/hiroendore_AC/main.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
 N, M = map(int, input().split())
-a = [int(input()) for i in range(M)]
-element = a[N%M]
-parity = int(N/M)
+a = [input() for i in range(M)]
+element = a[N%M-1]
+parity = int((N-1)/M)
 
 if parity%2 == 1 and element == "L":
     print("T")
