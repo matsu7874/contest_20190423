ffb7e0396204c91ac3bab228450baf0f2a36bb75
endo <hiroyuki_endo@forcia.com>
2019-04-21 21:32:43 +0900

    tansan WAの登録

---
+++ tansan/hiroendore_WA/main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+N, M = map(int, input().split())
+a = [input() for i in range(M)]
+element = a[N%M]
+parity = int((N-1)/M)
+
+if parity%2 == 1 and element == "L":
+    print("T")
+elif parity%2 == 1 and element == "T":
+    print("L")
+else:
+    print(element)
--- tansan/matsu7874_WA/main.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python3
-
-a, b, c = map(int, input().split())
-print(min(a*b+c, a+b*c))
