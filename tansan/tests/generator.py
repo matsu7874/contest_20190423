8223646c23f32ecca13d8195fcc69092126219dd
endo <hiroyuki_endo@forcia.com>
2019-04-21 21:20:25 +0900

    tansan テストコード生成

--- tansan/tests/generator.py
+++ tansan/tests/generator.py
@@ -4,37 +4,50 @@ from collections import namedtuple
 import random
 
 
-MIN_V = 0
-MAX_V = 100
+MIN_N = 1
+MAX_N = 1000000000
 
-Case = namedtuple('Case', ('prefix', 'a', 'b', 'c'))
+MIN_M = 1
+MAX_M = 100000
+
+MIN_a = 1
+MAX_a = 100000
+
+class TestCase:
+    def __init__(self, _p, N, M, a):
+        self._p = _p
+        self.N = N
+        self.M = M
+        self.a = a
 
 
 def main():
     num_testcase = 40
-    testcases = set()
+    testcases = []
 
     # sample case
-    testcases.add(Case('00_sample', 3, 5, 7))
-    testcases.add(Case('00_sample', 98, 99, 100))
+    testcases.append(TestCase('00_sample', 5, 3, ["L", "T", "T"]))
+    testcases.append(TestCase('00_sample', 100, 5, ["T", "L", "T", "L", "T"]))
 
     # corner case
-    testcases.add(Case('10_corner', 0, 1, 100))
+    #testcases.add(Case('10_corner', 0, 1, 100))
 
     # random case
     size_testcase = len(testcases)
     while size_testcase < num_testcase:
-        a = random.randint(MIN_V, MAX_V-2)
-        b = random.randint(a+1, MAX_V-1)
-        c = random.randint(b+1, MAX_V)
-        if Case('20_random', a, b, c) not in testcases:
-            testcases.add(Case('20_random', a, b, c))
+        N = random.randint(MIN_N, MAX_N)
+        M = random.randint(MIN_M, MAX_M)
+        a = random.choices(["T", "L"], k=M)
+        if TestCase('20_random', N, M, a) not in testcases:
+            testcases.append(TestCase('20_random', N, M, a))
             size_testcase += 1
 
     # prefixでソートして出力
-    for i, (_p, a, b, c) in enumerate(sorted(list(testcases))):
+    for i, case in enumerate(testcases):
         with open('input_{}.in'.format(i), 'w') as fout:
-            fout.write('{} {} {}\n'.format(str(a), str(b), str(c)))
+            fout.write('{} {}\n'.format(str(case.N), str(case.M)))
+            for i in range(case.M):
+                fout.write('{}\n'.format(str(case.a[i])))
 
 
 if __name__ == "__main__":
--- tansan/tests/validator.py
+++ tansan/tests/validator.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 import generator
 
-a, b, c = map(int, input().split())
-assert generator.MIN_V <= a < b < c <= generator.MAX_V, (a, b, c)
+N, M = map(int, input().split())
+a = [input() for i in range(M)]
+assert generator.MIN_N <= N <= generator.MAX_N, generator.MIN_N
+assert generator.MIN_M <= M <= generator.MAX_M, generator.MIN_M
